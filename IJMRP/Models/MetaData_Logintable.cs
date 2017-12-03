ba7a407ff9079d5cda54d5e981ea258ed7367436
pravat_sahoo <pravat.sahoo.15@gmail.com>
2017-12-03 18:46:40 +0530

    add project

---
+++ IJMRP/Models/MetaData_Logintable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IJMRP.Models
+{
+
+    [MetadataType(typeof(MetaData_Logintable))]
+    public partial class tblUserLogin
+    {
+
+    }
+    public class MetaData_Logintable
+    {
+        public string U_USERID { get; set; }
+        public string U_USERNAME { get; set; }
+        public string U_MOBILE { get; set; }
+        public string U_EMAIL { get; set; }
+        public string U_PASSWORD { get; set; }
+        public string U_ROLE { get; set; }
+    }
+}
